a555a33c16eb6c09df2b16cb0d5a040295b8db91
tobiisaravia <tttobis@gmail.com>
2022-03-10 15:40:05 -0300

    delete directions

--- client/src/components/Sections/ContactUs/Main.js
+++ client/src/components/Sections/ContactUs/Main.js
@@ -2,14 +2,13 @@ import React from 'react'
 import Form from './Form'
 import people from '../../../assets/ContactUs/people.png'
 import { Container, LeftContainer, ImageContainer, Image, RightContainer } from './Contact.elements'
-import Directions from './Directions/Directions'
+//import Directions from './Directions/Directions'
 const Main = () => {
     return (
         <Container>
             <LeftContainer>
                 <ImageContainer>
                     <Image src={people} alt='people'/>
-                    <Directions/>
                 </ImageContainer>
             </LeftContainer>
             <RightContainer>
